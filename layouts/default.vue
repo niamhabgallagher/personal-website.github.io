48f7422662299eca3a02067db6f2ccdcc61508cb
Niamh Gallagher <niamhgallagher@niamhs-air.home>
2024-01-29 21:54:00 +0000

    Added Tabs

--- layouts/default.vue
+++ layouts/default.vue
@@ -4,12 +4,37 @@
             <v-toolbar-title style="color: #fff; font-weight: bold;">
                 Niamh Gallagher
             </v-toolbar-title>
+
+            <v-tabs
+            v-model="tab"
+            align-tabs="title"
+            style="justify-content: center;"
+            >
+                <v-tab
+                    v-for="item in tabs"
+                    :key="item"
+                    :value="item"
+                >
+                    {{ item }}
+                </v-tab>
+            </v-tabs>
         </v-app-bar>
         <v-main>
             <slot />
         </v-main>
     </v-app>
 </template>
+
+<script>
+export default {
+    data() {
+        return {
+            tab: 0,
+            tabs: [ 'About Me', 'Work', 'Contact Me' ]
+        }
+    }
+}
+</script>
 
 <style>
 .v-application {
